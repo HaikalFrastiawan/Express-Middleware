f72e654d35ac77672e337fd0f3b64e9b1301cc07
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-14 13:04:17 +0700

    3.cara membuat middleware

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ morgan = require('morgan');
 
 
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    console.log('Hello from middleware!');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/Halaman', (req, res) => {
+    res.send('Halaman Page');
+} );
 
 
 app.listen(3000, () => {
