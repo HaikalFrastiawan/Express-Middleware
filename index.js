37e880b2d44608b53d23ec3e06a15be03938f872
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-14 12:55:15 +0700

    2.cara menggunakan midleware

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+morgan = require('morgan');
+
+
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
+
+
+
+module.exports = app;   
